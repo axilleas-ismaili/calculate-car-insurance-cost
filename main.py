4dd17fb0afdba1ff467a06574f7d9be175542902
Axilleas Ismaili <74646111+ismaAXILLEAS@users.noreply.github.com>
2021-01-28 23:32:11 +0200

    Calculate yearly insurance based on engine discplacement and user age.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 if __name__ == '__main__':
     engine_displacement = input('Type the engine displacement: ')
+    engine_displacement = int(engine_displacement)
     user_age = input('Type your age: ')
+    user_age = int(user_age)
+
+    if engine_displacement <= 1000:
+        yearly_insurance = 150
+    elif engine_displacement <= 2000:
+        yearly_insurance = 200
+    else:
+        yearly_insurance = 300
+
+    if user_age <= 23:
+        yearly_insurance = yearly_insurance + 40
+    '''
+  
+  fpa =  24 / 100 * etisia_asfalistra
+  etisia_asfalistra = fpa + etisia_asfalistra     
+  '''
+    
